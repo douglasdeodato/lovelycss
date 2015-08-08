66cc6d072d3ea73bee1307916d7fb68943d88e5d
Douglas Deodato <douglasdeodato@gmail.com>
2015-08-08 08:10:00 +0100

    add and run uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,26 @@ module.exports = function (grunt) {
 					'fast-one/public/stylesheets/style-min.css':['fast-one/public/stylesheets/style.css']
 				}
 			}
+		},
+
+		//uglify
+
+		uglify: {
+			dist: {
+				files:{
+					'with-bootstrap/public/js/plugins-min.js':['with-bootstrap/public/js/plugins.js']
+				}
+			}
 		}
+
+
 	});
 
 	//load the plugin
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	//do the task
-	grunt.registerTask('default',['cssmin']);
+	grunt.registerTask('default',['cssmin', 'uglify']);
 
 };
