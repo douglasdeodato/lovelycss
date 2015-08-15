62484fca4089dfce31f3065285a682e18683ee4e
Douglas Deodato <douglasdeodato@gmail.com>
2015-08-15 14:21:44 +0100

    improvements

--- old-Gruntfile.js
+++ old-Gruntfile.js
@@ -1,4 +1,43 @@
-	module.exports = function  (grunt) {
+// module.exports = function (grunt) {
+
+// 	//Configure main project
+// 	grunt.initConfig({
+// 		//settings about plugins
+// 		pkg: grunt.file.readJSON('package.json'),
+
+// 		//name of plugin 
+// 		cssmin: {
+// 			combine: {
+// 				files:{
+// 					'fast-one/public/stylesheets/style-min.css':['fast-one/public/stylesheets/style.css']
+// 				}
+// 			}
+// 		},
+
+// 		//uglify
+
+// 		uglify: {
+// 			dist: {
+// 				files:{
+// 					'with-bootstrap/public/js/plugins-min.js':['with-bootstrap/public/js/plugins.js']
+// 				}
+// 			}
+// 		}
+
+
+// 	});
+
+// 	//load the plugin
+// 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+// 	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+// 	//do the task
+// 	grunt.registerTask('default',['cssmin', 'uglify']);
+
+// };
+
+
+module.exports = function  (grunt) {
 	var config = {};
 
 
@@ -44,5 +83,11 @@
 	tasks.forEach(grunt.loadNpmTasks);
 
 };
+
+
+
+
+
+
 
 
