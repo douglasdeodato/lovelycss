0cf8bf6907177090b91c9cd7261f762016752819
Douglas Deodato <douglasdeodato@gmail.com>
2015-11-26 12:38:03 +0000

    old folder deleted

--- old/with-bootstrap/public/js/main.js
+++
@@ -1,7 +0,0 @@
-var data = require("./test");
-
-//test for sub, used for ember
-console.log(data['name']);
-console.log(data.name);
-
-console.log(data);
--- old/with-bootstrap/public/js/test.js
+++
@@ -1,5 +0,0 @@
-foo =1;
-
-module.exports = {
-	name:"douglas deodato"
-};
